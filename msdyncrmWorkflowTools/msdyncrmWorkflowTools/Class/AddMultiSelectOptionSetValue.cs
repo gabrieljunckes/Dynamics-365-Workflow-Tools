fa43d6d5975f67b07010d09565d50a24865f1fd5
Gabriel Dias Junckes <Gabriel.Junckes@theta.co.nz>
2018-07-22 17:23:36 +1200

    Add MultiSelect OptionSet Functionalities

---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/AddMultiSelectOptionSetValue.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Linq;
+
+namespace msdyncrmWorkflowTools
+{
+    public class AddMultiSelectOptionSetValue : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Values")]
+        [Default("126530000;126530001")]
+        public InArgument<string> Values { get; set; }
+
+        [RequiredArgument]
+        [Input("AttributeName")]
+        public InArgument<string> AttributeName { get; set; }
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+
+            #endregion
+
+            try
+            {
+                var values = Values.Get(executionContext).Split(';');
+                var attributeName = AttributeName.Get(executionContext).ToLower();
+
+                // Add on Trace
+                objCommon.tracingService.Trace("Values: {0}", Values.Get(executionContext));
+                objCommon.tracingService.Trace("attributeName: {0}", attributeName);
+
+                var entity = objCommon.service.Retrieve(objCommon.context.PrimaryEntityName,
+                                                        objCommon.context.PrimaryEntityId,
+                                                        new ColumnSet(attributeName));
+
+                var optionSetValueCollection = entity.GetAttributeValue<OptionSetValueCollection>(attributeName);
+                var newEntity = new Entity(objCommon.context.PrimaryEntityName, objCommon.context.PrimaryEntityId);
+
+                newEntity.Attributes[attributeName] = AddValuesInOptionSetValueCollection(values, optionSetValueCollection);
+
+                objCommon.tracingService.Trace("Update record");                
+                objCommon.service.Update(newEntity);
+            }
+            catch (Exception ex)
+            {
+                objCommon.tracingService.Trace("Message: {0} \nStackTrace: {1}", ex.Message, ex.StackTrace);
+                throw;
+            }
+        }
+
+        private OptionSetValueCollection AddValuesInOptionSetValueCollection(string[] values, OptionSetValueCollection optionSetValueCollection)
+        {
+            if (optionSetValueCollection == null) optionSetValueCollection = new OptionSetValueCollection();
+
+            foreach (var value in values)
+            {
+                if (!optionSetValueCollection.Any(x => x.Value.ToString() == value))
+                {
+                    optionSetValueCollection.Add(new OptionSetValue(int.Parse(value)));
+                }
+            }
+
+            return optionSetValueCollection;
+        }
+    }
+}
---
+++ msdyncrmWorkflowTools/msdyncrmWorkflowTools/Class/CompareMultiSelectOptionSet.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Linq;
+
+namespace msdyncrmWorkflowTools
+{
+    public class CompareMultiSelectOptionSet : CodeActivity
+    {
+        [Output("Result")]
+        public OutArgument<bool> Result { get; set; }
+
+        [RequiredArgument]
+        [Input("Values")]
+        [Default("126530000;126530001")]
+        public InArgument<string> Values { get; set; }
+
+        [RequiredArgument]
+        [Input("Condition")]
+        [Default("And, Or, Null")]
+        public InArgument<string> Condition { get; set; }
+
+        [RequiredArgument]
+        [Input("AttributeName")]
+        public InArgument<string> AttributeName { get; set; }
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            #region "Load CRM Service from context"
+
+            Common objCommon = new Common(executionContext);
+            objCommon.tracingService.Trace("Load CRM Service from context --- OK");
+
+            #endregion
+
+            try
+            {
+                var values = Values.Get(executionContext).Split(';');
+                var attributeName = AttributeName.Get(executionContext).ToLower();
+                var condition = Condition.Get(executionContext).ToLower();
+
+                // Add on Trace
+                objCommon.tracingService.Trace("Values: {0}", Values.Get(executionContext));
+                objCommon.tracingService.Trace("attributeName: {0}", attributeName);
+                objCommon.tracingService.Trace("condition: {0}", condition);
+
+                var entity = objCommon.service.Retrieve(objCommon.context.PrimaryEntityName,
+                                                        objCommon.context.PrimaryEntityId,
+                                                        new ColumnSet(attributeName));
+
+                var optionSetValueCollection = entity.GetAttributeValue<OptionSetValueCollection>(attributeName);
+                var result = GetConditionResult(condition, values, optionSetValueCollection);
+
+
+                Result.Set(executionContext, result);
+            }
+            catch (Exception ex)
+            {
+                objCommon.tracingService.Trace("Message: {0} \nStackTrace: {1}", ex.Message, ex.StackTrace);
+                throw;
+            }
+        }
+
+        private bool GetConditionResult(string condition, string[] values, OptionSetValueCollection optionSetValueCollection)
+        {
+            if (optionSetValueCollection == null) optionSetValueCollection = new OptionSetValueCollection();
+
+            switch (condition)
+            {
+                case "and":
+                    return (from o in optionSetValueCollection
+                            join v in values on o.Value.ToString() equals v
+                            select o).Count() == values.Count();
+
+                case "or":
+                    return (from o in optionSetValueCollection
+                            join v in values on o.Value.ToString() equals v
+                            select o).Count() > 0;
+
+                case "null":
+                    return optionSetValueCollection.Count == 0;
+
+                default:
+                    throw new ArgumentException("Condition is not found!");
+            }
+        }
+    }
+}
